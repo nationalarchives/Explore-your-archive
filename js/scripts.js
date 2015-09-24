4fa3229f3a2b53a1a9e7421eb08d5a4f5e056096
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2015-09-24 11:38:05 +0100

    In progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 // Drop down style
 var jQuery = jQuery.noConflict();
     jQuery('.chosen-select').select2();
+
+// Mobile nav drop down
+var jQuery = jQuery.noConflict();
+    jQuery( ".mobileNav .nav" ).hide();
+    jQuery(".box-shadow-menu").click(function(){
+        jQuery(".mobileNav .nav").slideToggle(500);
+});
