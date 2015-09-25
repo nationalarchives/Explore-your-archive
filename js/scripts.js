bea2ba7887d1c6b27f68d75e2d0bf8342ab50450
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2015-09-25 18:29:55 +0100

    In progress

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,12 @@ var jQuery = jQuery.noConflict();
     jQuery(".box-shadow-menu").click(function(){
         jQuery(".mobileNav .nav").slideToggle(500);
 });
+
+// Fade out loader background
+(function(jQuery) {
+    jQuery(window).load(function () {
+        jQuery("div.pace").fadeOut(); // will first fade out the loading animation
+        jQuery('div.loading-page').delay(650).fadeOut('slow');
+    });
+})(jQuery);
+
--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,12 +1,26 @@
 //jQuery to collapse the navbar on scroll
-var jQuery = jQuery.noConflict();
-jQuery(window).scroll(function() {
-    if (jQuery("header").offset().top > 50) {
-        jQuery("header").addClass("smaller");
-    } else {
-        jQuery("header").removeClass("smaller");
+(function($) {
+    var $header = $("header"),
+        $window = $(window);
+    $window.on('scroll touchstart', function() {
+        if ($header.offset().top > 50 || $window.width() < 1025) {
+            $header.addClass("smaller");
+        } else {
+            $header.removeClass("smaller");
+        }
+    });
+
+    function resize() {
+        if ($window.width() < 1024) {
+            return $header.addClass('smaller');
+        }
+
+        $header.removeClass('smaller');
     }
-});
+    $window
+        .resize(resize)
+        .trigger('resize');
+})(jQuery);
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 var jQuery = jQuery.noConflict();
